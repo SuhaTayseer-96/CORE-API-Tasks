56643b11454b53fae717c42aa4015dd138afcd34
SuhaKrasneh <suha.kras967@gmail.com>
2024-08-27 21:10:50 +0300

    updated

--- Put.post/WebAPICoreTasks/obj/Debug/net8.0/WebAPICoreTasks.AssemblyInfo.cs
+++ Put.post/WebAPICoreTasks/obj/Debug/net8.0/WebAPICoreTasks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPICoreTasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fd3137d91257ef6a874483d3f94b87e753f2d30")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPICoreTasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPICoreTasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
